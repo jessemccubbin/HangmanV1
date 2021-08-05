c3606b50499229566e717a09a0ba408a886b3661
Jesse Mccubbin <jessemccubbin1@gmail.com>
2021-08-05 13:22:54 +1000

    added scanner to read the txt file & stored the words into a ArrayList

--- src/com/jesse/Main.java
+++ src/com/jesse/Main.java
@@ -2,11 +2,18 @@ package com.jesse;
 
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
 
-        File words = new File("C:\\Users\\UST-User\\Desktop\\HangmanV1\\src\\com\\jesse\\engmix.txt");
+        File engmix = new File("C:\\Users\\UST-User\\Desktop\\HangmanV1\\src\\com\\jesse\\engmix.txt");
+
+        Scanner wordScanner = new Scanner(engmix);
+
+        ArrayList<String> words = new ArrayList<>();
+
     }
 }
