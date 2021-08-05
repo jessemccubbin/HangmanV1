4ec7f0a267900442b31158d1ba63747528b398d7
Jesse Mccubbin <jessemccubbin1@gmail.com>
2021-08-05 12:17:16 +1000

    added words' doc "engmix" & linked to main

--- src/com/jesse/Main.java
+++ src/com/jesse/Main.java
@@ -1,10 +1,12 @@
 package com.jesse;
 
 
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args){
 
+        File words = new File("C:\\Users\\UST-User\\Desktop\\HangmanV1\\src\\com\\jesse\\engmix.txt");
     }
 }
