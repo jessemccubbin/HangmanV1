fc17f5852f378aac4cb092168c29c1b7b6faefca
Jesse Mccubbin <jessemccubbin1@gmail.com>
2021-08-11 09:40:57 +1000

    big update. added a visual hangman, test class and a few more tweaks to main

---
+++ src/com/jesse/HangmanTest.java
@@ -0,0 +1,13 @@
+package com.jesse;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HangmanTest {
+
+    @Test
+    public void hangmanTest() {
+
+    }
+}
